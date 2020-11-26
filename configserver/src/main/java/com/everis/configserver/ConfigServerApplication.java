c27997ee2afa1cded5f7d6529f955797c70359d3
raulacera96 <raulacera96@usal.es>
2020-11-26 08:22:31 +0100

    Config server funcionando

--- configserver/src/main/java/com/everis/configserver/ConfigServerApplication.java
+++ configserver/src/main/java/com/everis/configserver/ConfigServerApplication.java
@@ -3,7 +3,9 @@ package com.everis.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
